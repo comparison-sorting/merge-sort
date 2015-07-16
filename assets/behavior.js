b8303ad824cedddfe514e4128947430fe115b1fe
aureooms <aurelien.ooms@gmail.com>
2015-07-16 17:42:35 +0000

    undefined

--- assets/behavior.js
+++ assets/behavior.js
@@ -226,6 +226,36 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
       "depth": 1,
       "outline": []
     }, {
+      "type": "folder",
+      "data": {
+        "path": "linkedlist",
+        "title": "linkedlist"
+      },
+      "depth": 1,
+      "children": [
+        {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".js"],
+              "pygmentsLexer": "javascript",
+              "multiLineComment": ["/*", "*", "*/"],
+              "singleLineComment": ["//"],
+              "ignorePrefix": "}",
+              "foldPrefix": "^",
+              "name": "JavaScript"
+            },
+            "sourcePath": "/home/aureooms/dev/js/js-mergesort/js/src/linkedlist/recursive.js",
+            "projectPath": "js/src/linkedlist/recursive.js",
+            "targetPath": "linkedlist/recursive",
+            "pageTitle": "linkedlist/recursive",
+            "title": "recursive"
+          },
+          "depth": 2,
+          "outline": []
+        }
+      ]
+    }, {
       "type": "file",
       "data": {
         "language": {
